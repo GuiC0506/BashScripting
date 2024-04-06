ab2c177df5f8ebbeb3b2a4688cf89872c1fab785
Guilherme China <echina725@gmail.com>
2024-04-06 13:47:34 -0300

    some stuff was made

--- script.sh
+++ script.sh
@@ -1,4 +1,30 @@
 #!/sbin/env bash
 
-echo "Hello world"
+# variables
+ami=$(whoami)
+echo $ami
+echo "Variables"
+name="Churros"
+echo "Hello $name"
+echo "Hello ${name}"
+echo "Hello ${#name}" # length of name
+echo "Hello ${dog:-"Shoyou"}" # default value if dog does not exist
+echo "Hello ${person:="Guilherme"}" # prints as default, and sets the value
+echo $person
+echo 'Hello $name'
+
+echo "----------------"
+#subshell - used for command substitution
+
+echo "Subshells"
+onePathBehind=$(cd ..; pwd)
+echo "one path behind: " + $onePathBehind
+echo "content going to file" >> textfile && echo "The file was created" && bat textfile && rm textfile  && echo "The file was removed"
+
+touch youcantseethecontent
+echo "A lucid content" >> youcantseethecontent
+shred youcantseethecontent
+
+
+
 
